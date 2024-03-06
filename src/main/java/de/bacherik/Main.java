6daa8ea8dca26977daf048265918188106ce7d66
BachErik <bacherik.info@gmail.com>
2024-03-06 15:54:57 +0100

    add some things

--- src/main/java/de/bacherik/Main.java
+++ src/main/java/de/bacherik/Main.java
@@ -1,11 +1,16 @@
 package de.bacherik;
 
+import lombok.Getter;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.utils.cache.CacheFlag;
 
 public class Main {
+    @Getter
+    private static Main instance;
     public static void main(String[] args) {
+        instance = new Main();
+        System.out.println("Starting bot...");
         JDABuilder builder = JDABuilder.createDefault(args[0]);
 
         // Disable parts of the cache
@@ -16,5 +21,6 @@ public class Main {
         builder.setActivity(Activity.watching("TV"));
 
         builder.build();
+        System.out.println("Bot started");
     }
 }
